f1c96f3ac44ff3a004891b6b8804a9fd671b27be
TomasBorquez <tomasborquez13@gmail.com>
2025-03-04 16:19:31 -0300

    Add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Steps 
+1 - Create `/build` folder
+2 - Download raylib and set at `C:/raylib`
+3 - Run `./build.sh`
--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include "raylib.h"
 
-int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 400
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+void update() {
+}
 
-  SetTargetFPS(60);
+void draw() {
+  ClearBackground(RAYWHITE);
+  DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+}
+
+int main() {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib");
+
+  SetTargetFPS(144);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     {
-      ClearBackground(RAYWHITE);
-      DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+      draw();
     }
     EndDrawing();
   }
